69ad28d86eda267d4da9e696ce92512c4c5495d4
Alepherite <alepherite@gmail.com>
2026-06-24 11:34:39 +0700

    chore: merge asset files into single index.html

--- ui/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-    if (window.go_backend_xu_ly) {
-        window.go_backend_xu_ly("init").then(res => {
-            console.log("Backend response:", res);
-        });
-    } else {
-        console.error("go_backend_xu_ly is not defined.");
-    }
-};
